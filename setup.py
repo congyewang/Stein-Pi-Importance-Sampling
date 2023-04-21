9a71acffcd1abc73dd46d21dba24bb900295be4a
Congye Wang <w.congye2@newcastle.ac.uk>
2023-04-21 17:39:56 +0100

    Change setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 
 setup(
-    name='stein_q_thinning',
+    name='stein_pi_thinning',
     version='0.1.0',
-    description='Library of Stein Q Thinning Algorithm',
-    url='https://github.com/congyewang/Stein-Q-Thinning',
+    description='Library of Stein Pi Thinning Algorithm',
+    url='https://github.com/congyewang/Stein-Pi-Thinning',
     author='Congye Wang',
     license='MIT',
-    packages=['stein_q_thinning'],
+    packages=['stein_pi_thinning'],
     install_requires=['jax', 'numpy', 'scipy', 'statsmodels', 'tqdm']
     )
